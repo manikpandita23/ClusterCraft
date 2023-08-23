014ce2619fd261d526ad90a904fae08975851a2f
Suryateja Challa <challasuryateja29@gmail.com>
2023-08-23 12:05:31 +0530

    Few changes

--- app.py
+++ app.py
@@ -4,8 +4,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.metrics import pairwise_distances
 
-# Scikit-learn algorithm
-from sklearn.cluster import KMeans as km
 # Custom algorithm
 from kmeans_v2 import CustomKMeans as ckm
 
@@ -17,27 +15,12 @@ def list_image_files(folder_path):
 def segment_image(selected_image_path):
     image = plt.imread(selected_image_path)
 
-    plt.figure(figsize=(15, 5))
+    plt.figure(figsize=(10, 5))
     plt.title('Original Image')
     plt.imshow(image)
     plt.show()
 
-    num_clusters = int(input("Clusters: "))
-
-    # Scikit-learn segmentation
-    X = image.reshape(-1, 3)
-
-    scikit_start = time.time()
-    kmeans_scikit = km(n_clusters=num_clusters, n_init=10)
-    kmeans_scikit.fit(X)
-    segmented_img_1 = kmeans_scikit.cluster_centers_[kmeans_scikit.labels_]
-    segmented_img_1 = segmented_img_1.reshape(image.shape)
-    scikit_end = time.time()
-
-    plt.figure(figsize=(15, 5))
-    plt.title('Segmented Image (Scikit-learn)')
-    plt.imshow(segmented_img_1)
-    plt.show()    
+    num_clusters = int(input("Clusters (more than 4 and less than 18): "))    
 
     # Custom segmentation
     n_pixels = image.shape[0] * image.shape[1]
@@ -50,30 +33,19 @@ def segment_image(selected_image_path):
     segmented_img_2 = kmeans_custom.centroids[cluster_labels].reshape(image.shape)
     custom_end = time.time()
 
-    plt.figure(figsize=(15, 5))
-    plt.title('Segmented Image (Custom)')
-    plt.imshow(segmented_img_2)
-    plt.show()
-
-    plt.figure(figsize=(15, 5))
+    plt.figure(figsize=(10, 5))
 
     # Original image
-    plt.subplot(131)
+    plt.subplot(121)
     plt.title("Original Image")
     plt.imshow(image)
-    plt.axis('off')
-
-    # Segmentation using scikit-learn KMeans
-    plt.subplot(132)
-    plt.title("Segmentation (scikit-learn KMeans)")
-    plt.imshow(segmented_img_1.astype(np.uint8))
-    plt.axis('off')
+    plt.axis('on')
 
     # Segmentation using custom KMeans
-    plt.subplot(133)
+    plt.subplot(122)
     plt.title("Segmentation (custom KMeans)")
     plt.imshow(segmented_img_2.astype(np.uint8))
-    plt.axis('off')
+    plt.axis('on')
 
     # Adjust spacing between subplots
     plt.tight_layout()
@@ -81,7 +53,6 @@ def segment_image(selected_image_path):
     # Show the plot
     plt.show()
 
-    print(f"Scikit-learn KMeans took {scikit_end - scikit_start} seconds.")
     print(f"Custom KMeans took {custom_end - custom_start} seconds.")
 
 images_folder = 'C:/Users/teja1/Downloads'
